7c48483b2192d3dffb4a72fe6e0c87060f01d793
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-05-21 20:32:48 -0300

    dois cards - cada tipo de chamada

--- app/src/main/java/com/example/usandoviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/usandoviewmodel/MainActivity.kt
@@ -6,44 +6,49 @@ import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var number: Int=0
+    private var number: Int = 0
     lateinit var viewModel: MyViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-       // showMyData()
-        soma()
+        showMyData()
+        somaFromViewModel()
+        somaFromView()
         showMyDataFromViewModel()
     }
 
-    private fun soma() {
-        btnSoma.setOnClickListener {
-            //number += 1
-          //  updateMyData()
-            updateMyDataFromViewModel()
+    private fun somaFromView() {
+        btnSomaView.setOnClickListener {
+            number ++
+            updateMyData()
         }
     }
 
-    private fun showMyData(){
-         receivedMessage.text = number.toString()
+    private fun showMyData() {
+        receivedMessageView.text = number.toString()
     }
 
-    private fun updateMyData(){
-         receivedMessage.text = number.toString()
+    private fun updateMyData() {
+        receivedMessageView.text = number.toString()
     }
 
     //________________Pegando da viewModel____________
 
-    private fun updateMyDataFromViewModel(){
+    private fun updateMyDataFromViewModel() {
         val myValorUpdated = viewModel.updateSoma().toString()
         receivedMessage.text = myValorUpdated
     }
 
-    private fun showMyDataFromViewModel(){
+    private fun showMyDataFromViewModel() {
         val myInitialValor = viewModel.initialValor().toString()
         receivedMessage.text = myInitialValor
     }
 
+    private fun somaFromViewModel() {
+        btnSoma.setOnClickListener {
+            updateMyDataFromViewModel()
+        }
+    }
 }
