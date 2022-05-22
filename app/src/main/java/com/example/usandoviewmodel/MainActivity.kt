4eda4f3ad2c22fc73930324552dbf1a94bbe4721
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-05-21 21:37:04 -0300

    deixa apenas a chamada da view

--- app/src/main/java/com/example/usandoviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/usandoviewmodel/MainActivity.kt
@@ -2,21 +2,16 @@ package com.example.usandoviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var number: Int = 0
-    lateinit var viewModel: MyViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
         showMyData()
-        somaFromViewModel()
         somaFromView()
-        showMyDataFromViewModel()
     }
 
     private fun somaFromView() {
@@ -32,23 +27,5 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateMyData() {
         receivedMessageView.text = number.toString()
-    }
-
-    //________________Pegando da viewModel____________
-
-    private fun updateMyDataFromViewModel() {
-        val myValorUpdated = viewModel.updateSoma().toString()
-        receivedMessage.text = myValorUpdated
-    }
-
-    private fun showMyDataFromViewModel() {
-        val myInitialValor = viewModel.initialValor().toString()
-        receivedMessage.text = myInitialValor
-    }
-
-    private fun somaFromViewModel() {
-        btnSoma.setOnClickListener {
-            updateMyDataFromViewModel()
-        }
     }
 }
